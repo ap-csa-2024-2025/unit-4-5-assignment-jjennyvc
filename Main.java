b49d481c355290771f2dd8317ee1288c5a8c7a1a
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2024-11-18 19:20:17 +0000

    skibidi toilet

--- Main.java
+++ Main.java
@@ -9,17 +9,38 @@ public class Main
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String length = word.substring(i, i+1);
+      for (int repeatTimes = 0; repeatTimes < N; repeatTimes++)
+      {
+      System.out.println(length);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int i = 10; i > 0; i --)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int i = 1; i <= 5; i++)
+    {
+      for (int j = 1; j <= i; j++)
+      {
+        System.out.print(j);
+      }
+      System.out.print();
+    }
   }
 
   public static void starTree()
